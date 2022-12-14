125e66b42a72a28e04c1298c95995f06ff59383a
Ed Conrad <ed.conrad@dymaptic.com>
2022-12-14 13:01:44 -0700

    Initial commit

---
+++ dymaptic.Pro.ZoomToCoordinates/Converters/ScaleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace dymaptic.Pro.ZoomToCoordinates.Converters
+{
+	public class ScaleConverter : IValueConverter
+	{
+
+		// Value is the Scale value from the ViewModel while targetType is the value in the View
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (Double.Parse((string)parameter) == (double) value);
+		}
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value ? parameter : Binding.DoNothing;
+		}
+	}
+}
