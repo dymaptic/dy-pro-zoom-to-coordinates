e79aaa7cc6b59df9b685c30dac9b617551b9d0a9
Ed Conrad <ed.conrad@dymaptic.com>
2023-01-05 17:38:49 -0700

    namespace cleanup

--- dymaptic.Pro.ZoomToCoordinates/ZoomToCoordinatesModule.cs
+++ dymaptic.Pro.ZoomToCoordinates/ZoomToCoordinatesModule.cs
@@ -1,28 +1,11 @@
-﻿using ArcGIS.Core.CIM;
-using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
-using ArcGIS.Desktop.Framework;
+﻿using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
-using ArcGIS.Desktop.Framework.Dialogs;
-using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.Layouts;
-using ArcGIS.Desktop.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace dymaptic.Pro.ZoomToCoordinates
 {
 	internal class ZoomToCoordinatesModule : Module
 	{
-		private static ZoomToCoordinatesModule _this = null;
+		private static ZoomToCoordinatesModule? _this;
 
 		/// <summary>
 		/// Retrieve the singleton instance to this module here
