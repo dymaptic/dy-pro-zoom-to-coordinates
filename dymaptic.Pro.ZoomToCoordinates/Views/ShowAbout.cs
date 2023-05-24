84bc760a44b50000a42a80b08749e55b9ad58888
Ed Conrad <ed.conrad@dymaptic.com>
2023-05-24 08:04:19 -0600

    Added about dymaptic info.

---
+++ dymaptic.Pro.ZoomToCoordinates/Views/ShowAbout.cs
@@ -0,0 +1,26 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace dymaptic.Pro.ZoomToCoordinates.Views
+{
+    internal class ShowAbout : Button
+    {
+
+        private About? _about = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_about != null)
+                return;
+            _about = new About
+            {
+                Owner = FrameworkApplication.Current.MainWindow
+            };
+            _about.Closed += (o, e) => { _about = null; };
+            _about.Show();
+            //uncomment for modal
+            //_about.ShowDialog();
+        }
+    }
+}
