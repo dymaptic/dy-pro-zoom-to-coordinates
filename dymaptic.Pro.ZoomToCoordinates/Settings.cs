6d005a9b8c48c8c0a45469e3095d61ad18a4b31c
Ed Conrad <ed.conrad@dymaptic.com>
2025-04-14 11:39:25 -0600

    auto-properties for settings

--- dymaptic.Pro.ZoomToCoordinates/Settings.cs
+++ dymaptic.Pro.ZoomToCoordinates/Settings.cs
@@ -5,18 +5,18 @@ namespace dymaptic.Pro.ZoomToCoordinates;
 public class Settings
 {
     // Default settings when Add-In loaded for initial time; user's actual default choices as they modify the drop-downs get saved/updated by EventHandler in ZoomToCoordinatesModule.cs 
-    public double Longitude = -122.4774494;
-    public double Latitude = 37.8108275;
-    public bool ShowFormattedCoordinates = false;
-    public double Scale = 10_000;
-    public bool ShowGraphic = false;
-    public string Marker = "Pushpin";
-    public string MarkerColor = "Green";
-    public int MarkerSize = 15;
-    public string FontFamily = "Tahoma";
-    public int FontSize = 11;
-    public string FontStyle = "Regular";
-    public string FontColor = "Black";
+    public double Longitude { get; set; } = -122.4774494;
+    public double Latitude { get; set; } = 37.8108275;
+    public bool ShowFormattedCoordinates { get; set; } = false;
+    public double Scale { get; set; } = 10_000;
+    public bool ShowGraphic { get; set; } = false;
+    public string Marker { get; set; } = "Pushpin";
+    public string MarkerColor { get; set; } = "Green";
+    public int MarkerSize { get; set; } = 15;
+    public string FontFamily { get; set; } = "Tahoma";
+    public int FontSize { get; set; } = 11;
+    public string FontStyle { get; set; } = "Regular";
+    public string FontColor { get; set; } = "Black";
 
     // Note: user doesn't get the option to change the default CoordinateFormat 
     public CoordinateFormat CoordinateFormat { get; } = CoordinateFormat.DecimalDegrees;
